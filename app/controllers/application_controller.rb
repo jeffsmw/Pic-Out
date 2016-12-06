a0951dbdae75f54e87f06d21cc1fad29f0599209
Jeffrey Wong <jeffsmwong@gmail.com>
2016-12-06 09:58:04 -0800

    Getting JSON from Instagram API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  attr_accessor :search
+
   def authenticate_user
     redirect_to new_session_path, alert: 'Please sign in' unless user_signed_in?
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get '/' => 'home#index', as: :home
+  get '/search' => 'home#show', as: :search
 
   resources :users
   resources :sessions, only: [:destroy, :create, :new] do
     delete :destroy, on: :collection
   end
+
 end
