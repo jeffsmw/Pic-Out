934eae4084a3dd85545ab986bf8f6bee1453c745
Jeffrey Wong <jeffsmwong@gmail.com>
2016-12-13 15:30:08 -0800

    AJAX, Lightbox, Restaurant: page, maps, images

--- app/assets/javascripts/channels/loading.js
+++ app/assets/javascripts/channels/loading.js
@@ -3,11 +3,8 @@ App.loading = App.cable.subscriptions.create("LoadingChannel", {
   disconnected: function() {},
   received: function(data) {
     return $('#channel').append(
-      // '<a href="https://www.instagram.com/p/'+ data['message']+
-      // '"><img src="'+data['thumb']+
-      // '" height="100" width="100"/></a>'
       '<img src="'+data['thumb']+
-      '" height="100" width="100" onClick="lightbox(this.src)"/>'
+      '" class="search-image" onClick="lightbox(this.src)"/>'
     );
   },
   speak: function(data_request) {
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,11 @@ Rails.application.routes.draw do
 
   get '/' => 'home#index', as: :home
   # get '/search' => 'home#show', as: :search
-  post({'/'    => 'home#create', as: :search})
+  post({'/' => 'home#create', as: :search})
 
   resources :users
   resources :sessions, only: [:destroy, :create, :new] do
     delete :destroy, on: :collection
   end
-  resources :restaurants, only: [:show]
-
+  resources :restaurants, only: [:show, :create]
 end
